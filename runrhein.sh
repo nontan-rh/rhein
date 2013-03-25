e41560e6b9495503d6fb4f0d21cf76994045cb17
Nontan <nzmiszk@gmail.com>
2013-03-26 00:28:32 +0900

    Change bytecode input from stdin to file

--- runrhein.sh
+++ runrhein.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-gosh -I src/compiler rheinc $1 | gosh -I src/assembler vmasm | rheinvm
+BCTEMP=`mktemp tmp.bc.XXXXXX`
+gosh -I src/compiler rheinc $1 | gosh -I src/assembler vmasm > $BCTEMP
+
+rheinvm $BCTEMP
+rm $BCTEMP
 
--- src/vm/vmmain.cc
+++ src/vm/vmmain.cc
@@ -11,11 +11,25 @@
 using namespace rhein;
 using namespace rhein::basic;
 
-int main() {
+int main(int argc, char** argv) {
     GC_init();
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: rheinvm <bytecode>\n");
+        return 1;
+    }
+
     State *state = new (GC_malloc(sizeof(State))) State();
-    state->loadFile(stdin);
     state->loadModule(BasicModule::create(state));
+
+    FILE* fp = fopen(argv[1] ,"r");
+    if (fp == nullptr) {
+        fprintf(stderr, "Cannot open bytecode file\n");
+        return 1;
+    }
+    state->loadFile(fp);
+    fclose(fp);
+
     execute(state, state->string_provider->getString("main"), 0, nullptr);
     return 0;
 }
